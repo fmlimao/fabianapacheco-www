d02859bddd8f778d298bd54b9603aabd04bb792f
Renan <renan.dasilva@jsl.com.br>
2022-03-24 11:46:22 -0300

    Configurando layout e cookies

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
+console.clear()
+
+require('dotenv').config()
+
 const express = require('express')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Fabiana Pacheco - Site em construção')
+app.use(cookieParser())
+app.set('views', './src/views')
+app.set('view engine', 'ejs')
+
+const authMiddleware = (req, res, next) => {
+  const login = req.cookies.login || null
+  res.locals.login = login
+
+  next()
+}
+
+app.get('/', authMiddleware, (req, res) => {
+  res.render('home', {
+    title: 'Fabiana Pacheco',
+    login: res.locals.login
+  })
 })
 
-app.listen(10300, () => {
-  console.log('Server is running on port 10300')
+app.get('/login', authMiddleware, (req, res) => {
+  res.cookie('login', {
+    id: 123456,
+    name: 'Lele'
+  })
+  res.redirect('/')
+})
+
+app.get('/logout', authMiddleware, (req, res) => {
+  res.clearCookie('login')
+  res.redirect('/')
+})
+
+const { PORT = 3000 } = process.env
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
 })
